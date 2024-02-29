779fe85750e6e383744f116b878d801e277c06d3
UmangPatel011 <157251213+UmangPatel011@users.noreply.github.com>
2024-02-28 19:57:54 -0500

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='bottom'>
+       <div className="center">
+        All rights reserved &copy; Umang Patel
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
